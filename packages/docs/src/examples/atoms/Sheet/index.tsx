894492cea18af44297e28f2315a9f3f37a0a16b9
kyuhak yuk <kyuhakyuk@kyuhaks-MBP-2.localdomain>
2022-03-01 15:14:53 -0800

    feat: add new component, Sheet

---
+++ packages/docs/src/examples/atoms/Sheet/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Snack from "../../../components/ComponentDoc/Snack";
+
+const SheetExamples = () => {
+  return (
+    <>
+      <Snack id="@kyuhakyuk/sheet" />
+    </>
+  );
+};
+
+export default SheetExamples;
