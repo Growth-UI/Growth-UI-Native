161c23ce5379ea805173069db34b5e06dcfa3804
kyuhak yuk <kyuhakyuk@kyuhaks-MacBook-Pro-2.local>
2022-03-17 12:59:02 -0700

    fix: add fill prop

--- packages/growth-ui-native/src/atoms/Icon/Lock.tsx
+++ packages/growth-ui-native/src/atoms/Icon/Lock.tsx
@@ -3,7 +3,7 @@ import { Svg, Path, SvgProps } from "react-native-svg";
 
 export default function Lock(props: SvgProps) {
   return (
-    <Svg viewBox="0 0 24 24" height="25" width="25" {...props}>
+    <Svg viewBox="0 0 24 24" height="25" width="25" fill="currentColor" {...props}>
       <Path fill="none" d="M0 0h24v24H0V0z"></Path>
       <Path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM9 8V6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9z"></Path>
     </Svg>
--- packages/growth-ui-native/src/atoms/Icon/Phone.tsx
+++ packages/growth-ui-native/src/atoms/Icon/Phone.tsx
@@ -3,7 +3,7 @@ import { Path, Svg, SvgProps } from "react-native-svg";
 
 export default function Phone(props: SvgProps) {
   return (
-    <Svg viewBox="0 0 24 24" height="25" width="25" {...props}>
+    <Svg viewBox="0 0 24 24" height="25" width="25" fill="currentColor" {...props}>
       <Path fill="none" d="M0 0h24v24H0z"></Path>
       <Path d="M15.5 1h-8A2.5 2.5 0 005 3.5v17A2.5 2.5 0 007.5 23h8a2.5 2.5 0 002.5-2.5v-17A2.5 2.5 0 0015.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"></Path>
     </Svg>
