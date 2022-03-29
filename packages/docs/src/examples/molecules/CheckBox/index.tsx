887940ce5b1e807f45e3e4f743cd8afd43b041af
kyuhak yuk <kyuhakyuk@kyuhaks-MBP-2.localdomain>
2022-03-29 09:44:42 -0700

    docs: add doc for CheckBox component

---
+++ packages/docs/src/examples/molecules/CheckBox/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Snack from "../../../components/ComponentDoc/Snack";
+
+const CheckBox = () => {
+  return (
+    <>
+      <Snack id="@kyuhakyuk/checkbox" />
+    </>
+  );
+};
+
+export default CheckBox;
