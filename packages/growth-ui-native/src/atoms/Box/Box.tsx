47675353e540f2ee0d96f4802b6f91ca3e5b74f6
kyuhakyuk <kyuhak.yuk@icloud.com>
2022-10-07 11:54:17 -0700

    feat: add dark mode

--- packages/growth-ui-native/src/atoms/Box/Box.tsx
+++ packages/growth-ui-native/src/atoms/Box/Box.tsx
@@ -1,4 +1,6 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useContext } from "react";
+import theme from "../../theme";
+import ThemeContext from "../../ThemeContext";
 import { sx } from "../../utils";
 import { SX } from "../../types";
 import { View } from "react-native";
@@ -6,9 +8,12 @@ import { View } from "react-native";
 const Box: FunctionComponent<BoxProps> = (props) => {
   const { children, ...rest } = props;
 
+  const { mode } = useContext(ThemeContext);
+
   return (
     <View
       style={{
+        backgroundColor: theme[mode].backgroundColor,
         ...sx(props.sx),
       }}
       {...rest}
