885928df5599fda2a0ed2deaf34508ec7b908b7b
kyuhakyuk <kyuhak.yuk@icloud.com>
2022-12-24 13:01:05 -0800

    fix: different style for search icon

--- packages/growth-ui-native/src/atoms/Icon/Search.tsx
+++ packages/growth-ui-native/src/atoms/Icon/Search.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Svg, Path, SvgProps } from "react-native-svg";
+import { G, Path, Svg, SvgProps } from "react-native-svg";
 
 export default function Search(props: SvgProps) {
   return (
-    <Svg viewBox="0 0 50 50" height="30" width="30" fill="currentColor" {...props}>
-      <Path d="M23 36c-7.2 0-13-5.8-13-13s5.8-13 13-13 13 5.8 13 13-5.8 13-13 13zm0-24c-6.1 0-11 4.9-11 11s4.9 11 11 11 11-4.9 11-11-4.9-11-11-11z"></Path>
-      <Path d="M32.682 31.267l8.98 8.98-1.414 1.414-8.98-8.98z"></Path>
+    <Svg viewBox="0 0 24 24" height="48" width="48" fill="currentColor" {...props}>
+      <G data-name="Layer 2">
+        <Path
+          d="m20.71 19.29-3.4-3.39A7.92 7.92 0 0 0 19 11a8 8 0 1 0-8 8 7.92 7.92 0 0 0 4.9-1.69l3.39 3.4a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 11a6 6 0 1 1 6 6 6 6 0 0 1-6-6z"
+          data-name="search"
+        ></Path>
+      </G>
     </Svg>
   );
 }
